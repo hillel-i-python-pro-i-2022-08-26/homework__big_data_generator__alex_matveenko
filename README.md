771f397e733c14f66e69abddd108955ec18ee0ed
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-10-03 23:04:51 +0300

    change code with the latest clarification, rewrite readme

--- README.md
+++ README.md
@@ -11,21 +11,9 @@
 * `d-homework-i-run` - *run docker*
 * `d-homework-i-purge` - *run all actions needed for purge project and related data*
 
-## Create two function. Each with own way to generate data
+#### `def generate_users()`:
 
-1. `def big_data_generator__set` - *it use set to save data*
-2. `def big_data_generator__list` - *it use list to save data*
-
-#### Version with set (`def big_data_generator_set`):
-
-* **Fast**
+* **Use set to save data**
 * **Excludes repetitions**
-* **Passed 1_000_000 users generate**
-* **function is generator that output info with `def output_data__set`**
-
-#### Version with list (`def big_data_generator__list`):
-
-* **Too slow**
-* **Passed 200_000 users generate**
-* **Return number of repeat login and password**
-* **function is generator that output info with `def output_data__list`**
+* **Passed 950_000 unique users generate**
+* **Use to output object Person**
--- generate_users.py
+++ generate_users.py
@@ -1,64 +1,51 @@
 import random
-from typing import Iterator
+from typing import Iterator, Protocol, TypeAlias
 
 from faker import Faker
 
 fake = Faker()
 
-
-# Version_use_set_for_generate
-def big_data_generator__set(number: int) -> Iterator:
-    username: set[str] = set()
-    user_password: set[str] = set()
-    while len(username) != number and len(user_password) != number:
-        fake_name = (
-            f"{fake.name().split()[0].lower()}_{random.randint(1, 9999)}_"
-            f"{fake.name().split()[1].lower()}_{random.randint(1, 9999)} "
-        )
-        fake_password = f"{fake.password()}{random.randint(0, 1000)}{fake.password()}"
-        username.add(fake_name)
-        user_password.add(fake_password)
-    for count, (name, password) in enumerate(zip(username, user_password), start=1):
-        yield f"{count}. Login: {name}. Pass: {password}"
+T_LOGIN: TypeAlias = str
+T_PASSWORD: TypeAlias = str
 
 
-# Version_use_list_for_generate_and_count_repeats
-def big_data_generator__list(number: int) -> Iterator:
-    username: list[str] = []
-    user_password: list[str] = []
-    repeat_names: int = 0
-    repeat_password: int = 0
-    for login in range(number):
-        login = f"{fake.first_name().lower()}_{random.randint(1, 9999)}_{fake.name().split()[1].lower()}"
-        if login in username:
-            login += f"{random.randint(1, 9999)}"
-            repeat_names += 1
-        username.append(login)
-    for password in range(number):
-        password = f"{fake.password()}{random.randint(0, 1000)}"
-        if password in user_password:
-            password += f"{fake.password().reverse}{random.randint(0, 1000)}"
-            repeat_password += 1
-        user_password.append(password)
-
-    for item, (name, password) in enumerate(zip(username, user_password), start=1):
-        yield f"{item}.Login:{name}. Pass:{password}\nLogin repeat:{repeat_names}.Password repeat:{repeat_password}"
+class UserProtocol(Protocol):
+    login: T_LOGIN
+    password: T_PASSWORD
 
 
-# Organize_output_for_set_version
-def output_data__set(amount: int) -> str:
-    data = big_data_generator__set(amount)
-    return "\n".join([f"{item}" for item in data])
+class Person:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
 
 
-# Organize_output_for_list_version
-def output_data__list(amount: int) -> str:
-    data = big_data_generator__list(amount)
-    return "\n".join([f"{item}" for item in data])
+def validate(users: list[UserProtocol], amount: int) -> None:
+    logins = set(map(lambda user: user.login, users))
+    if amount != (amount_of_logins := len(logins)):
+        raise ValueError(f'Not enough of unique items. Required: "{amount}". Provided: "{amount_of_logins}"')
+
+
+def generate_users(amount: int) -> Iterator[UserProtocol]:
+    login: set[str] = set()
+    password: set[str] = set()
+    # Generate_info__start
+    while len(login) != amount and len(password) != amount:
+        fake_name = f"{fake.profile()['username']}_{fake.first_name().lower()}"
+        fake_password = f"{fake.password()}{random.randint(1, 9999)}"
+        login.add(fake_name)
+        password.add(fake_password)
+    # Generate_info__stop
+
+    for name, pas in zip(login, password):
+        yield Person(login=name, password=pas)
+
+
+def main():
+    amount = 100_000
+    users = list(generate_users(amount=amount))
+    validate(users=users, amount=amount)
 
 
 if __name__ == "__main__":
-    # Version with set
-    print(output_data__set(1), end="\n\n")  # Add "end" for readability
-    # Version with list(much longer)
-    print(output_data__list(1))
+    main()
