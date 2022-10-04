c116b3401efbb7339ec41efa1ce1bb09a8898808
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-10-04 21:40:01 +0300

    change login generate

--- generate_users.py
+++ generate_users.py
@@ -31,7 +31,7 @@ def generate_users(amount: int) -> Iterator[UserProtocol]:
     password: set[str] = set()
     # Generate_info__start
     while len(login) != amount and len(password) != amount:
-        fake_name = f"{fake.profile()['username']}_{fake.first_name().lower()}"
+        fake_name = f"{fake.profile()['username']}_{fake.name().split()[1].lower()}"
         fake_password = f"{fake.password()}{random.randint(1, 9999)}"
         # Generate_info__stop
 
